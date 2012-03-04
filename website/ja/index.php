9d377e48c8e819c969460ff8a819ef802b1dff05
Gordon McDowell <gordonmcdowell@gmail.com>
2012-03-03 17:41:26 -0700

    Adding Japanese support. Very basic.

---
+++ website/ja/index.php
@@ -0,0 +1,5 @@
+<?php
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: http://thoriumremix.com/ja/2011/");
+exit();
+?> 
---
+++ website/ja/teac3.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ja" xml:lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=640px" />
+<link rel="stylesheet" type="text/css" media="screen"  href="/style.css" /><title>ワシントンDCで開かれた第三回トリウム・エナジー・アライアンス会合での講演</title>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-25859838-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script></head>
+<body>
+<div id="Box">
+<div id="Header">
+<h1 class="main_title">トリウム溶融塩炉による日本のリバイバル戦略</h1>
+<ul class="tabs">
+<li><a href="/ja/2011/">2011<br />&nbsp;</a></li>
+<li id="Selected"><a href="/ja/teac3/">TEAC3<br />&nbsp;</a></li>
+</ul><br /><br />
+</div>
+
+<div class="text">
+ワシントンDCで開かれた第三回トリウム・エナジー・アライアンス会合での講演 <a href="http://thoriumenergyalliance.com">ThoriumenEnergyAlliance.com
+</a>
+</div>
+
+<div class="space">&nbsp;</div>
+
+<div class="video">
+
+<object width="640" height="390"><param name="movie" value="http://www.youtube.com/v/3TQNuaZ23j0?fs=1&hl=ja&rel=0&cc_load_policy=1&cc_lang_pref=ja&version=3"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/3TQNuaZ23j0?fs=1&hl=ja&rel=0&cc_load_policy=1&cc_lang_pref=ja&version=3" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="640" height="390"></embed></object>
+
+</div>
+
+
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+
+<div class="text" style="text-align:center;">
+<font style="white-space:nowrap;">private contact: <a href="mailto:THORIUMREMIX@GMAIL.COM">email</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</font> <font style="white-space:nowrap;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; public contact: <a href="http://www.facebook.com/ThoriumRemix">facebook</a> <a href="http://www.youtube.com/user/thoriumremix">youtube</a> <a href="http://twitter.com/#!/thoriumremix">twitter</a> <a href="http://www.reddit.com/r/ThoriumRemix/">reddit</a></font>
+</div>
+
+<ul class="language-tabs">
+<li><a href="/hr/">croatian</li>
+<li><a href="/">english</a></li>
+<li><a href="/fr/">french</a></li>
+<li><a href="/de/">german</a></li>
+<li id="Selected"><a href="/ja/">japanese</a></li>
+<li><a href="/lv/">latvian</a></li>
+<li><a href="/es/">spanish</a></li>
+</ul>
+
+</div>
+</body>
+</html>
