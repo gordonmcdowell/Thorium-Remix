9fdc06b4509df396923c6e9fcad8f10a305faf87
Gordon McDowell <gordonmcdowell@gmail.com>
2012-03-07 14:25:45 -0700

    german TEDxYYC

---
+++ website/de/tedxyyc.php
@@ -0,0 +1,632 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="de" xml:lang="de">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=640px" />
+<link rel="stylesheet" type="text/css" media="screen"  href="/style.css" /><title>TEDxYYC - Thorium</title>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-25859838-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script></head>
+<body>
+<div id="Box">
+<div id="Header">
+<h1 class="main_title">Der Thorium-Flüssigsalzreaktor</h1>
+<ul class="tabs">
+<li><a href="/de/2011/">2011<br />&nbsp;</a></li>
+<li id="Selected"><a href="/de/tedxyyc/">TEDxYYC<br />&nbsp;</a></li>
+</ul><br /><br />
+</div>
+
+<div class="text">
+Thorium ist ein in der Natur vorkommender Kernbrennstoff.
+Es gibt viermal mehr Thorium in der Erdkruste als Uran. Und man kann es in
+Thorium-Flüssigsalzreaktoren einsetzen, um Elektrizität zu erzeugen, Hitze
+und weitere wertvolle Produkte.
+
+
+</div>
+
+<div class="space">&nbsp;</div>
+
+<div class="video">
+
+<object width="640" height="390"><param name="movie" value="http://www.youtube.com/v/N2vzotsvvkw?fs=1&hl=de&rel=0&cc_load_policy=1&cc_lang_pref=de&version=3"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/N2vzotsvvkw?fs=1&hl=de&rel=0&cc_load_policy=1&cc_lang_pref=de&version=3" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="640" height="390"></embed></object>
+
+</div>
+
+<div class="space">&nbsp;</div>
+
+
+<div class="text">
+Fast jeder von uns ist Teil einer
+Gemeinschaft, sei sie groß oder klein,
+
+und alle Gemeinschaften
+haben ähnliche Bedürfnisse:
+
+Sie brauchen Licht,
+sie brauchen Wärme,
+
+sie brauchen Klimaanlagen.
+Menschen funktionieren nicht sehr gut,
+
+wenn es zu heiß ist
+oder zu kalt.
+
+Sie brauchen Lebensmittel, die wachsen,
+verteilt und sicher gelagert werden.
+
+Müll muss einsammelt, entfernt
+und aufbereitet werden.
+
+Menschen müssen von einem Ort
+zum anderen kommen können –
+
+und das möglichst schnell.
+
+Für dies alles ist
+Energieversorgung die Grundlage.
+
+Energie in Form von Elektrizität
+gibt uns Licht und Klima,
+
+Energie in Form von Wärme hält uns warm,
+und Energie in chemischer Form 
+
+ liefert Kunstdünger und treibt
+Landwirtschaftsmaschinen und Verkehr an.
+
+Ich habe 10 Jahre lang
+bei der NASA gearbeitet.
+
+In der ersten Zeit dort habe ich mich
+sehr für Gemeinschaften interessiert.
+
+Aber die Art von Gemeinschaft, die mich
+beschäftigt hat, war diese hier:
+
+eine Mondkolonie. Sie hat die gleichen Bedürfnisse
+wie ein Gemeinwesen auf der Erde,
+
+aber ein paar
+sehr spezielle Einschränkungen.
+
+Und wir müssen uns überlegen, wie wir eine solche
+spezielle Gemeinschaft mit Energie versorgen können.
+
+Es gibt keine Kohle auf dem Mond,
+es gibt kein Öl.
+
+Es gibt kein Erdgas.
+Es gibt keine Atmosphäre.
+
+Es gibt auch keinen Wind.
+
+Und die Solarenergie hat ein echtes Problem: Der
+Mond braucht einen Monat für eine Erdumkreisung.
+
+Zwei Wochen lang gibt es keine Sonne, und
+die Solar-Paneele erzeugen keine Energie.
+
+Genügend Energie für zwei Wochen
+in Akkus zu speichern,
+
+ist schlicht nicht durchführbar.
+
+Daher ist Kernenergie
+die einzige Möglichkeit.
+
+Damals, im Jahr 2000, wusste ich nicht
+wirklich viel über Kernenergie.
+
+Darum versuchte ich, möglichst alles
+über Kernenergie zu lernen,
+
+wie wir sie heute einsetzen. Überall auf der
+Welt verwenden wir Wasser als Kühlmittel.
+
+Das hat einige Vorteile,
+aber es hat auch viele Nachteile.
+
+Wenn Sie Elektrizität erzeugen wollen,
+
+müssen Sie das Wasser viel stärker erhitzen,
+als Sie es normalerweise können.
+
+Unter Normaldruck siedet Wasser
+bei 100 Grad Celsius.
+
+Das ist bei weitem nicht heiß genug,
+um effektiv Elektrizität zu erzeugen.
+
+Darum müssen wassergekühlte Reaktor
+bei Drücken laufen, die sehr viel höher sind
+
+als der normale Luftdruck.
+
+Einige wassergekühlte Reaktoren
+arbeiten bei über 70 bar,
+
+andere sogar bei einem
+Druck von 150 bar.
+
+Das lässt sich nicht verhindern.
+Das muss man halt so machen,
+
+wenn man mit einem wassergekühlten
+Reaktor Strom erzeugen will.
+
+Und das bedeutet: Einen wassergekühlten
+Reaktor muss man als Druckbehälter bauen –
+
+mit Stahlwandungen, die über 20 Zentimeter dick sind.
+Das hört sich schwer an? Das ist es auch!
+
+Schlimm wird es bei einem Unfall
+mit Druckverlust im Reaktor.
+
+Wenn Sie flüssiges Wasser bei 300 Grad Celsius haben
+und der Druck fällt plötzlich weg,
+
+dann bleibt das Wasser nicht sehr lange flüssig:
+Es wird schlagartig zu Dampf!
+
+Darum baut man wassergekühlte Reaktoren
+in großen, dicken Gebäuden aus Beton,
+
+sogenannten Containments.
+
+Das Containment soll den Wasserdampf aufnehmen,
+der aus dem Reaktor kommt,
+
+wenn es einen Unfall mit Druckverlust gibt.
+
+Wasserdampf braucht tausendmal mehr
+Volumen als flüssiges Wasser.
+
+Daher ist das Containment sehr groß
+im Vergleich zum Reaktor.
+
+Bei einem Druckverlust gibt es ein weiteres
+großes Problem, wenn das Wasser verdampft.
+
+Ohne Notkühlung kann der Kernbrennstoff
+im Reaktor überhitzen und schmelzen.
+
+Die heutigen Reaktoren verwenden
+Uranoxid als Brennstoff.
+
+Das ist ein keramisches Material und
+verhält sich ähnlich wie die Keramik,
+
+aus der wir Kaffeetassen oder Kochgeschirr
+machen oder Ziegel für den Kamin.
+
+Sie sind chemisch stabil,
+aber keine guten Wärmeleiter.
+
+Bei einem Druckverlust und
+einem Verlust des Kühlwassers
+
+werden die Brennelemente bald schmelzen und die
+radioaktiven Spaltprodukte freisetzen, die darin enthalten sind.
+
+Die Herstellung eines festen Kernbrennstoffs ist
+eine komplizierte und teure Angelegenheit.
+
+Außerdem verwenden wir nur weniger als 1 Prozent
+der Energie, die im Brennstoff enthalten ist.
+
+Danach können die Brennelemente
+nicht länger im Reaktor bleiben.
+
+Wassergekühlte Reaktoren stellen
+eine weitere Anforderung:
+
+Sie müssen in der Nähe großer Wassermengen sein,
+damit der Dampf, den sie erzeugen,
+
+gekühlt und kondensiert werden kann.
+Andernfalls können sie keinen Strom erzeugen.
+
+Auf dem Mond gibt es aber keine Seen oder Flüsse.
+Und wenn Sie jetzt sagen, dass wassergekühlte Reaktoren 
+
+ für eine Mondkolonie doch wohl keine
+so gute Idee sind, würde ich Ihnen zustimmen.
+
+Aber wissen Sie, ich hatte das Glück, etwas
+über eine andere Art von Kernenergie zu lernen,
+
+die diese Probleme alle nicht hat, und zwar aus einem ganz
+einfachen Grund: sie arbeitet nicht mit Wasserkühlung, 
+
+ und sie arbeitet nicht mit festen Brennstoffen.
+Verblüffenderweise arbeitet sie mit Salz.
+
+Eines Tages bei der Arbeit war ich im Büro
+eines Freundes und sah ein Buch in seinem Regal:
+
+»Reaktoren mit Flüssigbrennstoffen«. Das interessierte mich,
+und ich fragte, ob ich es ausleihen könne.
+
+Ich las über Forschungsarbeiten damals in den 1950er
+Jahren in den Vereinigten Staaten, die sich mit einem 
+
+Kernreaktor befassten, der nicht mit festem Brennstoff oder
+Wasserkühlung arbeitete und der die Probleme 
+
+eines wassergekühlten Reaktors nicht hatte.
+Der Grund dafür war sehr bestechend:
+
+Er verwendete eine Mischung von
+Fluoridsalzen als Kernbrennstoff,
+
+speziell die Fluoride von Lithium,
+Beryllium, Uran und Thorium.
+
+Fluoride sind chemisch außerordentlich stabil.
+
+Sie reagieren nicht mit Luft oder Wasser,
+Sie müssen sie auf 400 Grad Celsius aufheizen, 
+
+ um sie zum Schmelzen zu bringen,
+und das ist geradezu perfekt, 
+
+ um in einem Kernreaktor Strom zu erzeugen.
+
+Und jetzt kommt der Clou: Diese Reaktoren
+brauchen nicht mit hohen Drücken zu arbeiten.
+
+Und das ist der größte Unterschied.
+
+Das bedeutet: Sie brauchen nicht in
+großen, schweren Stahlkesseln zu sein.
+
+Sie brauchen kein Wasser als Kühlmittel,
+und es gibt nichts im Reaktor, 
+
+das – anders als Wasser –
+seine Dichte nennenswert verändert.
+
+Darum kann das Containment um den Reaktor 
+
+sehr viel kleiner und enganliegend sein.
+Anders als die Festbrennstoffe, die schmelzen können, 
+
+ wenn sie nicht gekühlt werden, sind die
+flüssigen Fluoridbrennstoffe bereits geschmolzen –
+
+und zwar bei einer viel, viel
+niedrigeren Temperatur.
+
+Im Normalbetrieb gibt es einen kleinen Pfropfen
+hier am Boden des Reaktorbehälters.
+
+Dieser Pfropfen besteht aus 
+
+ gefrorenem Salz.
+Er bleibt gefroren, 
+
+ weil kaltes Gas über die
+Außenseite des Rohres geblasen wird.
+
+Falls bei einem Notfall der gesamte
+Strom im Kernkraftwerk ausfällt, 
+
+ kann das Gebläse nicht mehr blasen, 
+
+ der gefrorene Stopfen schmilzt,
+und der flüssige Fluoridbrennstoff im Reaktor 
+
+ läuft aus dem Behälter heraus 
+
+ durch die Leitung in einen anderen
+Tank hinein, den Auffangtank.
+
+Das Innere des Auffangtanks ist auf
+maximale Wärmeabfuhr ausgelegt.
+
+Das Salz wird passiv gekühlt,
+und die Hitze geht mit der Zeit zurück.
+
+Bei wassergekühlten Reaktoren muss man das
+Kraftwerk im Allgemeinen mit Strom versorgen, 
+
+ um den Kühlkreislauf aufrechtzuerhalten und
+eine Kernschmelze wie in Japan zu verhindern.
+
+Aber wenn bei diesem Reaktor der Strom ausfällt,
+fährt sich der Reaktor selbst herunter.
+
+Er macht das ganz allein,
+ohne menschliches Zutun, 
+
+ und er bringt sich selbst in einen
+sicheren und kontrollierten Zustand.
+
+Das klang für mich alles sehr überzeugend.
+Es hat mich begeistert, welches Potential 
+
+ein Flüssigsalzreaktor zur
+Versorgung einer Mondkolonie bot.
+
+Aber dann habe ich von Thorium erfahren,
+und die Geschichte wurde noch besser.
+
+Thorium ist ein in der Natur vorkommender Kernbrennstoff.
+Es gibt viermal mehr Thorium in der Erdkruste 
+
+als Uran. Und man kann es in
+Thorium-Flüssigsalzreaktoren einsetzen,
+
+um Elektrizität zu erzeugen, Hitze
+und weitere wertvolle Produkte.
+
+Thorium hat eine so hohe Energiedichte,
+dass Sie die Energie für Ihr ganzes Leben 
+
+ in der Hand halten können.
+
+Auch auf dem Mond ist Thorium ein ganz
+gewöhnlicher Stoff und leicht zu finden.
+
+Diese Karte zeigt, wo auf dem Mond
+Thorium zu finden ist.
+
+Thorium hat eine elektromagnetische Signature,
+durch die es leicht zu finden ist, 
+
+ sogar von einem Raumschiff aus.
+
+Mit der Energie eines Thorium-Flüssigsalzreaktors
+könnten wir die Luft recyclen und auch 
+
+ Wasser und Müll in der Mondkolonie
+wiederverwenden. Das wäre ja auch ein 
+
+ absolutes Muss, um erfolgreich zu sein.
+
+We die Pflanzen, die wir zur Ernährung der Menschen brauchen,
+auch in der zwei Wochen langen 
+
+ Mondnacht wachsen lassen:
+mit Licht und Strom aus dem Reaktor.
+
+Es sah ganz so aus, als ob der Thorium-Flüssigsalzreaktor –
+oder LFTR – genau die Energiequelle ist, die eine 
+
+ autarke Mondkolonie Wirklichkeit werden lassen könnte.
+Aber ich hatte eine einfache Frage:
+
+Wenn das eine derart großartige Sache
+für eine Gemeinschaft auf dem Mond ist,
+
+warum dann nicht für eine Gemeinschaft auf der Erde?
+Für eine Gesellschaft der Zukunft: 
+
+ selbstversorgend und energieunabhängig.
+
+Dieselbe Art der Energieerzeugung
+und dieselben Recycling-Techniken,
+
+die großen Einfluß auf das
+Überleben auf dem Mond haben,
+
+könnten ebenso einen großen Einfluß
+auf das Überleben auf der Erde haben.
+
+Heute verbrennen wir fossile Brennstoffe, weil sie leicht
+zu finden sind, und einfach, weil wir es können.
+
+Leider sehen dadurch manche Teile
+unseres Planeten wie der Mond aus.
+
+Weil wir fossile Brennstoffe verwenden, werden wir in
+Konflikte in instabilen Regionen der Welt verwickelt.
+
+Wir zahlen mit Geld und mit Leben.
+
+Alles könnte sehr viel anders sein,
+wenn wir Thorium verwendeten.
+
+Wissen Sie, im LFTR können wir Thorium
+etwa 200 Mal effizienter einsetzen, 
+
+als wir es heute mit Uran machen,
+denn der LFTR ist in der Lage,
+
+die Energie im Thorium
+fast vollständig freizusetzen.
+
+Das vermindert die Abfallproduktion
+gegenüber Uran um das Hundertfache,
+
+und millionenfach gegenüber fossilen Brennstoffen.
+
+Wir brauchen auch weiterhin flüssige Kraftstoffe
+für Fahrzeuge und Maschinen, 
+
+ aber wir können die Flüssigkraftstoffe aus
+dem Kohlendioxid in der Atmosphäre 
+
+und aus Wasser gewinnen – ganz ähnlich, wie es die Natur macht.
+Wir können Wasserstoff durch Aufspalten von Wasser erzeugen 
+
+ und mit dem Kohlenstoff verbinden, den wir
+aus dem CO2 in der Atmosphäre gewonnen haben.
+
+So schaffen wir Kraftstoffe wie
+Methanol, Ammoniak und 
+
+ Dimethyläther, die ein direkter
+Ersatz für Diesel sein können.
+
+Stellen Sie sich kohlendioxidneutrales Benzin und Diesel vor –
+nachhaltig und selbst produziert.
+
+Haben wir denn genug Thorium?
+Ja, haben wir!
+
+Beispielsweise haben wir in den Vereinigten Staaten
+über 3.200 Tonnen Thorium,
+
+das vor 50 Jahren angesammelt wurde und heute
+in einem Flachgraben in Nevada liegt.
+
+Allein dieses Thorium könnte,
+in einem LFTR eingesetzt, 
+
+ so viel Energie erzeugen, wie
+die USA in drei Jahren verbrauchen.
+
+Thorium ist keine seltene Substanz.
+Es gibt viele Orte wie diesen hier in Idaho.
+
+Aus einer Fläche von der Größe eines Football-Felds
+könnte man genug Thorium gewinnen, 
+
+ um die ganze Welt
+ein Jahr mit Energie zu versorgen.
+
+Mit der LFTR-Technologie könnten wir wegkommen
+von den teuren und schwierigen Aspekten 
+
+ der heutige Kernenergie mit
+Wasserkühlung und festen Uranbrennelementen.
+
+Wir brauchen keine großen Hochdruckreaktoren,
+wir brauchen keine riesigen Containments!
+
+Wir brauchen keine großen,
+wenig effizienten Dampfturbinen.
+
+Wir brauchen nicht so viele Hochspannungsleitungen
+über große Entfernungen,
+
+Denn Thorium ist eine gut transportierbare Energiequelle,
+die man dort findet, wo man sie braucht.
+
+Ein Thorium-Flüssigsalzreaktor wäre
+eine kompakte Anlage, 
+
+ sehr energieeffizient und sicher. Sie könnte Tag und
+Nacht die Energie produzieren, die wir brauchen – 
+
+ und zwar ohne Rücksicht
+auf das jeweilige Wetter.
+
+2007 haben wir 5 Milliarden Tonnen Kohle verbraucht,
+31 Milliarden Barrel Öl 
+
+ und 5 Billionen Kubikmeter Erdgas, 
+
+ außerdem 65.000 Tonnen Uran.
+Alles für den Weltenergiebedarf!
+
+Mit LFTR könnten wir dasselbe mit
+7.000 Tonnen Thorium erreichen.
+
+So viel könnte man in einer einzigen Mine abbauen.
+Wenn Sie dies alles interessant finden, 
+
+ lade ich Sie ein, unsere Website zu besuchen,
+wo eine wachsende, enthusiastische Gemeinschaft 
+
+ von Thorium-Befürwortern dabei ist,
+der Welt zu erzählen, 
+
+ wie wir eine saubere und nachhaltige
+Zukunft der Energie erreichen können – 
+
+ mit Energie aus Thorium.
+Vielen Dank!
+</div>
+
+
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+<div class="nothing">&nbsp;</div>
+
+<div class="text" style="text-align:center;">
+<font style="white-space:nowrap;">private contact: <a href="mailto:THORIUMREMIX@GMAIL.COM">email</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</font> <font style="white-space:nowrap;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; public contact: <a href="http://www.facebook.com/ThoriumRemix">facebook</a> <a href="http://www.youtube.com/user/thoriumremix">youtube</a> <a href="http://twitter.com/#!/thoriumremix">twitter</a> <a href="http://www.reddit.com/r/ThoriumRemix/">reddit</a></font>
+</div>
+
+<ul class="language-tabs">
+<li><a href="/es/">spanish</a></li>
+<li><a href="/lv/">latvian</a></li>
+<li><a href="/ja/">japanese</a></li>
+<li id="Selected"><a href="/de/">deutsch</a></li>
+<li><a href="/fr/">français</a></li>
+<li><a href="/">english</a></li>
+<li><a href="/hr/">croatian</a></li>
+
+</ul>
+
+
+</div>
+</body>
+</html>
